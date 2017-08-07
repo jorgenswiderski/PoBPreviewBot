a719180f136f70f0b70153b9fee2aeb1a5f47348
aggixx <aggixx.gaming@gmail.com>
2017-08-06 23:15:09 -0500

    Change max_deletion_check_interval to 1 day.

--- live_config.py
+++ live_config.py
@@ -8,5 +8,5 @@ min_pull_count = 5
 pull_count_tracking_window = 10
 max_pull_count = 200
 
-max_deletion_check_interval = 21600
+max_deletion_check_interval = 86400
 min_deletion_check_interval = 60
