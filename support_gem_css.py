bb5e2ab8eba41f67c4adcb0f6113adabc27e6dcc
aggixx <aggixx.gaming@gmail.com>
2018-10-24 15:31:52 -0500

    More concise CSS template (thanks Emmitt).

--- support_gem_css.py
+++ support_gem_css.py
@@ -12,37 +12,26 @@ css_str = """a[href$="#support-gem-red"], a[href$="#support-gem-green"], a[href$
     text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
     white-space: nowrap;  
 }
-a[href$="#support-gem-red"] {
+
+a[href$="#support-gem-red"],
+a[href$="#support-gem-red"]:hover:before {
     background-color: #c51e1e;
 }
 
-a[href$="#support-gem-green"] {
+a[href$="#support-gem-green"],
+a[href$="#support-gem-green"]:hover:before {
     background-color: #08a842;
 }
 
-a[href$="#support-gem-blue"] {
+a[href$="#support-gem-blue"],
+a[href$="#support-gem-blue"]:hover:before {
     background-color: #4163c9;
 }
 
-a[href$="#support-gem-red"]:hover:before {
-   position: absolute;
-   background: #c51e1e;
-   margin-top: -25px;
-   padding: 1px 3px;
-   z-index: 1;
-}
-
-a[href$="#support-gem-green"]:hover:before {
-   position: absolute;
-   background: #08a842;
-   margin-top: -25px;
-   padding: 1px 3px;
-   z-index: 1;
-}
-
+a[href$="#support-gem-red"]:hover:before,
+a[href$="#support-gem-green"]:hover:before,
 a[href$="#support-gem-blue"]:hover:before {
    position: absolute;
-   background: #4163c9;
    margin-top: -25px;
    padding: 1px 3px;
    z-index: 1;
