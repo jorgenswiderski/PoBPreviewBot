019f609eff7cc3cf5eeb69b53add0c5add54c9e0
aggixx <aggixx.gaming@gmail.com>
2019-04-08 11:14:09 -0500

    Enable screen logging in start script.

--- start.sh
+++ start.sh
@@ -3,8 +3,12 @@ cd /home/ec2-user/PoBPreviewBot
 
 if ! screen -list | grep -q ".bot"; then
 	#/usr/bin/screen -S bot -dmS /usr/local/bin/python /home/ec2-user/PoBPreviewBot/main.py
-	screen -S bot -dmS python main.py
+	#screen -S bot -dmS python main.py
+	screen -L -S bot -dmS python main.py
 else
 	#/usr/bin/screen -S bot -X /usr/local/bin/python /home/ec2-user/PoBPreviewBot/main.py
-	screen -S bot -X python main.py
+	#screen -S bot -X python main.py
+	screen -L -S bot -X python main.py
 fi
+
+#screen -S bot -X log on
