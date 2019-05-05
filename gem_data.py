6d9111f685cb6b7451cea58b03cee4d134d59b1b
aggixx <aggixx.gaming@gmail.com>
2019-05-04 21:11:57 -0500

    Convert gem data from TSV to JSON. Fixes #14.

--- gem_data.py
+++ gem_data.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import os
 import re
+import json
 
 class gem_color(Enum):
 	WHITE = 0
@@ -11,13 +12,15 @@ class gem_color(Enum):
 class gem_data_t:
 	url_suffix_re = re.compile(".com/(.+?)$")
 
-	def __init__(self, tsv_info):
-		info = tsv_info.split("\t")
-		self.name = info[0]
+	def __init__(self, json):
+		self.__dict__ = json
+		
+		# sanitize inputs
+		self.color_str = self.color_str.lower()
+		self.wiki_url = self.wiki_url.strip()
+		
+		# set some derived attributes
 		self.shortname = self.name.replace(" Support", "")
-		self.color_str = info[1].lower()
-		self.shortcode = info[2]
-		self.wiki_url = info[3].strip()
 		self.slug = self.name.replace(" ", "_")
 		
 		self.color = self.__parse_color__()
@@ -43,26 +46,36 @@ class gem_data_t:
 	def get_url_suffix(self):
 		search = self.url_suffix_re.search(self.wiki_url)
 		return search.group(1)
-		
 
+# Used to convert json.loads() output's keys and values from unicode to strings
+def byteify(input):
+    if isinstance(input, dict):
+        return {byteify(key): byteify(value)
+                for key, value in input.iteritems()}
+    elif isinstance(input, list):
+        return [byteify(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
+		
 def load_gems_from_file(path):
 	if not os.path.isfile(path):
 		raise Exception("Path to support gem info is invalid. {:s}".format(path))
 		
 	support_gems = {}
+	raw_data = None
 	
 	with open(path, "r") as f:
-		gems = f.read()
-		gems = gems.split("\n")
-		gems = filter(None, gems)
+		raw_data = byteify(json.loads(f.read()))
 		
-		for gem in gems:
-			gem = gem_data_t( gem )
-			support_gems[ gem.shortname.lower() ] = gem
+	for data in raw_data:
+		gem = gem_data_t(data)
+		support_gems[ gem.shortname.lower() ] = gem
 			
 	return support_gems
 	
-support_gems = load_gems_from_file("data/support_gems.tsv")
+support_gems = load_gems_from_file("data/support_gems.json")
 
 def get_support_gem_by_name(name):
 	if name in support_gems:
