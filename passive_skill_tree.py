73f32e9a9f7f68979114aeba5c30ebcb984c468f
aggixx <aggixx.gaming@gmail.com>
2020-04-06 11:39:46 -0500

    Improve method of loading skill tree data. Update all game data to patch 3.10.

--- passive_skill_tree.py
+++ passive_skill_tree.py
@@ -1,6 +1,22 @@
-from data.passive_skills import passiveSkillTreeData as data 
+# Python
+import logging
+import json
+
+path = 'data\\passive_skills.json'
+
+logging.debug("Loading skill tree data from '{}'...".format(path))
+
+with open(path) as f:
+	data = json.load(f)
 
 nodes = {}
 
+logging.debug("Parsing nodes from data...")
+
 for key, value in data['nodes'].iteritems():
-	nodes[int(key)] = value
+	try:
+		nodes[int(key)] = value
+	except ValueError as e:
+		logging.debug("Skipped passive skill node key '{}'".format(key))
+
+logging.debug("Skill tree data parsing complete.")
